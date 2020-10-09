e4874c48709114dd94e4fd17f26aaf9983c959b4
Don Osipov <donosipov@gmail.com>
2020-10-08 21:36:26 -0400

    created makefile and saved input into variables

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+# !/usr/bin/python
+import sys
+
+print(sys.argv)
+if len(sys.argv) < 4:
+    print('usage: ARGS="encode/decode ciphertext/plaintext keytext')
+    sys.exit()
+
+# whatToDo = open(sys.argv[1], "r")
+whatToDo = sys.argv[1]
+text = sys.argv[2]
+
+keyText = sys.argv[3]
+key = [list(keyText[i : i + 5]) for i in range(0, len(keyText), 5)]
+
+sys.exit()
+
+# print(key)
+
+# print(whatToDo)
+# print(text)
+# print(keyText)
+
+
 def addX(plaintext):
     xLength = 0
     for i in range(0, len(plaintext), 2):
